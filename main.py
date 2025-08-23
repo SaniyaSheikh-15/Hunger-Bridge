396e9cceffdf69341ddc02a5a9d388e3eecb347b
Ameya (JARVIS) Ramteke <ameya.jarvis07@gmail.com>
2025-08-23 14:16:44 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+import webbrowser
+import os
+
+# Path to your role-selection.html file
+file_path = os.path.abspath("role-selection.html")
+
+# Open the HTML file in the default web browser
+webbrowser.open(f"file:///D:/CODES/HTML/HUNGER_BRIDGE/role-selection.html")
